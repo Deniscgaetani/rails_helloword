01aa4c97925bf5b2df1a3d6aa1ff27a9a8683423
Gaetani <deniscgaetani@gmail.com>
2023-03-06 15:48:52 -0300

    second commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+end
