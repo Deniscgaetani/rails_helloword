912263e951b3b958392ad1d5765dfb2e24c3bf54
Gaetani <deniscgaetani@gmail.com>
2023-03-06 15:48:36 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
   get 'welcome/index'
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
